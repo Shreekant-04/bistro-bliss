90052ea70851348a8b132c3fe14a3ee1068a3c17
Shreekant <shreekant4062@gmail.com>
2025-02-01 12:40:29 +0530

    style: import global CSS styles in Layout component

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
+import "./index.css";
 
 const Layout = () => {
   return (
